4fa43bd2c6d6d1a21870f7982e3544699f478e72
Abby Fischler <abigail.fischler@gmail.com>
2022-07-21 15:19:59 -0400

    Updates

--- api/hello.js
+++ api/hello.js
@@ -2,6 +2,20 @@
 require('dotenv').config()
 // console.log(process.env) 
 
+
+
+//ack club bank API STUFF
+var axios = require("axios").default;
+
+var options = {
+    method: 'GET',
+    url: 'https://bank.hackclub.com/api/v3/organizations/hq/donations',
+    headers: {
+        'Content-Type': 'application/json'
+    }
+};
+
+
 // STEP 1:
 
 
@@ -28,54 +42,60 @@ const API_KEY = process.env.API_KEY;
 const DOMAIN = 'hackclub.com';
 
 const mailgun = require("mailgun-js");
-const mg = mailgun({apiKey: API_KEY, domain: DOMAIN});
+const mg = mailgun({
+    apiKey: API_KEY,
+    domain: DOMAIN
+});
 const data = {
-	from: 'Abby Fischler (the money queen) <abby@hackclub.com>',
-	to: 'abby@hackclub.com',
-	subject: 'Fre$h moneyyy',
-	text: 'Testing some Mailgun awesomness!'
+    from: 'Abby Fischler (the money queen) <abby@hackclub.com>',
+    to: 'abby@hackclub.com',
+    subject: 'Fre$h moneyyy',
+    text: 'Testing some Mailgun awesomness!'
 };
 
 
 const messageData = {
-  from: 'Abby Fischler <abby@hackclub.com>',
-  to: 'abigail.fischler@gmail.com',
-  subject: 'Donation@',
-  text: 'Hack Club has recived a donation!'
+    from: 'Abby Fischler <abby@hackclub.com>',
+    to: 'abigail.fischler@gmail.com',
+    subject: 'Donation@',
+    text: 'Hack Club has recived a donation!'
 };
 
 
-export default function handler(request, response) {
-  const { name } = request.query;
- mg.messages().send(data, function (error, body) {
-	console.log(error, body);
-  response.status(200).send(`Hello ${body}!`);
+export default async function hello(req, res) {
+   
+    // mg.messages().send(data, function(error, body) {
+    //         console.log(error, body);
+    //         response.status(200).send(`Hello ${body}!`);
+    //     }
+
+    //)
+    var data = await axios.request(options); 
+    var txs = data.data;
+    
+    data.forEach((tx) => {
+      console.log(tx);
+  });
+
+    //bank API output storing as data
+
+
+    console.log("HIII THIS CODE HAS RUN!");
+    res.send(data.data);
 }
 
-)
 
-console.log("HIII THIS CODE HAS RUN!");
-}
+ 
 
+// // goal: index through the first API array element
 
-//hack club bank API STUFF
-// var axios = require("axios").default;
+// how do i link so it goes through the API index?
+// function first(arr) {
+//     if(!Array.isArray(arr)) return;
+//     return arr[0];
+//  }
 
-// var options = {
-//   method: 'GET',
-//   url: 'https://bank.hackclub.com/api/v3/donations/donation_id',
-//   headers: {'Content-Type': 'application/json'}
-// };
-
-// axios.request(options).then(function (response) {
-//   console.log(response.data);
-// }).catch(function (error) {
-//   console.error(error);
-// });
-// module.exports = {
-//   handler: (request, response) => {
-//     const { name } = request.query;
-//     response.status(200).send(`Hello ${name}!`);
-//   }
-//   }
-
+//  // 2 attempt
+//  File yourFile = new File("transaction.txt");
+// yourFile.createNewFile(); // if file already exists will do nothing 
+// FileOutputStream oFile = new FileOutputStream(yourFile, false); 
