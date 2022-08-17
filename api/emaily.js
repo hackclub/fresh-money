2c7916c3dd206e13a6022823175c15dc25207168
Abby Fischler <abigail.fischler@gmail.com>
2022-08-17 15:17:18 -0700

    Final Iteration #1

--- api/emaily.js
+++ api/emaily.js
@@ -26,15 +26,23 @@ export default (req, res) => {
                 day: '2-digit'
             }).replace(/(\d+)\/(\d+)\/(\d+)/, '$3-$1-$2').toString()
         ));
+        var welcomeMessage = `Hi Christina! Hack Club has a lot going on in the bank. Here is your daily update. XX, Abby! \n`
+       var txs2 = txs.map(transaction => {
+       
+        return `Transaction Name: ${transaction.memo}, Amount: $${transaction.amount_cents/100}, Date: ${transaction.date}` 
+
+        }).join("\n")
         // var txt = txs.map(transaction => {
         //     if (transaction.amount_cents >= 100 * 10) {
         //         return `Donor Name: ${transaction.donor.name}, Amount: $${transaction.amount_cents/100}, Date: ${transaction.date}`
         //     }
         // });
 
-        res.send(txs);
-        sendEmail('abby@hackclub.com', 'abby@hackclub.com', `fresh money: ${Date.now()}`, txs)
-        // sendEmail('max@hackclub.com', 'abby@hackclub.com', sub, txt)
+        var emailMessage = welcomeMessage + txs2;
+        sendEmail('abby@hackclub.com', 'abby@hackclub.com', `Fresh money: ${Date.now()}`, (emailMessage))
+        sendEmail('christina@hackclub.com', 'abby@hackclub.com', `Fresh money: ${Date.now()}`, (emailMessage))
+        sendEmail('max@hackclub.com', 'abby@hackclub.com', `Fresh money: ${Date.now()}`, (emailMessage))
+       
     })
 } else {
     res.status(400).send("nope didn't work")
