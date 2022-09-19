7d9cb739aa21301fbe8bb35446fb5b8fc3b44eff
Max Wofford <max@maxwofford.com>
2022-09-19 12:06:22 -0400

    Format emaily.js

--- api/emaily.js
+++ api/emaily.js
@@ -15,38 +15,38 @@ const mg = mailgun({
 });
 
 export default (req, res) => {
-    if(req.query.date) {
-    axios.request(options).then(async data => {
-        var txs = data.data;
-        txs = txs.filter(tx => (
-            tx.date ===
-            (new Date(req.query.date)).toLocaleString('en-us', {
-                year: 'numeric',
-                month: '2-digit',
-                day: '2-digit'
-            }).replace(/(\d+)\/(\d+)\/(\d+)/, '$3-$1-$2').toString()
-        ));
-        var welcomeMessage = `Hi Christina! Hack Club has a lot going on in the bank. Here is your daily update. XX, Abby! \n`
-       var txs2 = txs.map(transaction => {
-       
-        return `Transaction Name: ${transaction.memo}, Amount: $${transaction.amount_cents/100}, Date: ${transaction.date}` 
+    if (req.query.date) {
+        axios.request(options).then(async data => {
+            var txs = data.data;
+            txs = txs.filter(tx => (
+                tx.date ===
+                (new Date(req.query.date)).toLocaleString('en-us', {
+                    year: 'numeric',
+                    month: '2-digit',
+                    day: '2-digit'
+                }).replace(/(\d+)\/(\d+)\/(\d+)/, '$3-$1-$2').toString()
+            ));
+            var welcomeMessage = `Hi Christina! Hack Club has a lot going on in the bank. Here is your daily update. XX, Abby! \n`
+            var txs2 = txs.map(transaction => {
 
-        }).join("\n")
-        // var txt = txs.map(transaction => {
-        //     if (transaction.amount_cents >= 100 * 10) {
-        //         return `Donor Name: ${transaction.donor.name}, Amount: $${transaction.amount_cents/100}, Date: ${transaction.date}`
-        //     }
-        // });
+                return `Transaction Name: ${transaction.memo}, Amount: $${transaction.amount_cents / 100}, Date: ${transaction.date}`
 
-        var emailMessage = welcomeMessage + txs2;
-        await sendEmail('abby@hackclub.com', 'abby@hackclub.com', `Fresh money: ${Date.now()}`, (emailMessage))
-        await sendEmail('christina@hackclub.com', 'abby@hackclub.com', `Fresh money: ${Date.now()}`, (emailMessage))
-        await sendEmail('max@hackclub.com', 'abby@hackclub.com', `Fresh money: ${Date.now()}`, (emailMessage))
-        res.status(200).send("it sent!!!! WOOOHOOOO")
-    })
-} else {
-    res.status(400).send("nope didn't work")
-}
+            }).join("\n")
+            // var txt = txs.map(transaction => {
+            //     if (transaction.amount_cents >= 100 * 10) {
+            //         return `Donor Name: ${transaction.donor.name}, Amount: $${transaction.amount_cents/100}, Date: ${transaction.date}`
+            //     }
+            // });
+
+            var emailMessage = welcomeMessage + txs2;
+            await sendEmail('abby@hackclub.com', 'abby@hackclub.com', `Fresh money: ${Date.now()}`, (emailMessage))
+            await sendEmail('christina@hackclub.com', 'abby@hackclub.com', `Fresh money: ${Date.now()}`, (emailMessage))
+            await sendEmail('max@hackclub.com', 'abby@hackclub.com', `Fresh money: ${Date.now()}`, (emailMessage))
+            res.status(200).send("it sent!!!! WOOOHOOOO")
+        })
+    } else {
+        res.status(400).send("nope didn't work")
+    }
     // res.send("sent email")
 }
 
@@ -58,7 +58,7 @@ function sendEmail(to, from, subject, text) {
         text: text
     };
 
-   
+
     return new Promise((resolve, reject) => {
         mg.messages().send(data, (error, body) => {
             if (error) {
