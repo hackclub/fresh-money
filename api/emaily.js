3098f58b376c1eed0e450c28e33392daf13bd663
Abby Fischler <46800781+abbyfischler@users.noreply.github.com>
2023-06-20 12:52:21 -0400

    added

--- api/emaily.js
+++ api/emaily.js
@@ -1,11 +1,4 @@
-var axios = require("axios").default;
-var options = {
-  method: 'GET',
-  url: 'https://bank.hackclub.com/api/v3/organizations/hq/transactions',
-  headers: {
-    'Content-Type': 'application/json'
-  }
-};
+import fetch from 'node-fetch';
 
 const mailgun = require("mailgun-js");
 const mg = mailgun({
@@ -15,47 +8,58 @@ const mg = mailgun({
 
 export default (req, res) => {
   if (req.query.date) {
-    axios.request(options).then(async data => {
-      var txs = data.data;
-      txs = txs.filter(tx => (
-        tx.date ===
-        (new Date(req.query.date)).toLocaleString('en-us', {
-          year: 'numeric',
-          month: '2-digit',
-          day: '2-digit'
-      }).replace(/(\d+)\/(\d+)\/(\d+)/, '$3-$1-$2').toString()
-      ));
-      var welcomeMessage = `Hi!! Here is your daily update: <br />`;
-      var endingMessage = `Best, Abby`;
-      var txs2 = txs.map(transaction => {
-        return `
-          <tr style="background-color: ${transaction.amount_cents > 0 ? "rgba(51,214,166,0.125)" : "rgba(236,55,80,0.125)"}; color: black; border-bottom: 1px solid rgba(0,0,0,0.0625)">
+    fetch('https://bank.hackclub.com/api/v3/organizations/hq/transactions')
+      .then(response => response.json())
+      .then(async data => {
+        var txs = data;
+        txs = txs.filter(tx => (
+          tx.date ===
+          (new Date(req.query.date)).toLocaleString('en-us', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit'
+          }).replace(/(\d+)\/(\d+)\/(\d+)/, '$3-$1-$2').toString()
+        ));
+        var welcomeMessage = `Hi!! Here is your daily update: <br />`;
+        var endingMessage = `Best, Abby`;
+        var txs2 = txs.map(transaction => {
+          return `
+          <tr style="background-color: ${
+            transaction.amount_cents > 0 ? "rgba(51,214,166,0.125)" : "rgba(236,55,80,0.125)"
+          }; color: black; border-bottom: 1px solid rgba(0,0,0,0.0625)">
             <td style="padding: 3px 10px;">${transaction.memo}</td>
-            <td style="padding: 3px 10px;">${(transaction.amount_cents / 100).toLocaleString('en-US', {style: 'currency', currency: 'USD'})}</td>
+            <td style="padding: 3px 10px;">${(transaction.amount_cents / 100).toLocaleString('en-US', {
+              style: 'currency',
+              currency: 'USD'
+            })}</td>
           </tr>
         `;
-      }).join("\n");
+        }).join("\n");
 
-      var currentDate = new Date().toLocaleDateString('en-us', {
-        year: 'numeric',
-        month: 'long',
-        day: 'numeric'
-      }).toString();
+        var currentDate = new Date().toLocaleDateString('en-us', {
+          year: 'numeric',
+          month: 'long',
+          day: 'numeric'
+        }).toString();
 
-      var subject = getSubjectForDay(currentDate);
+        var subject = getSubjectForDay(currentDate);
 
-      var html = `${welcomeMessage}<br /><table style="border-collapse: collapse;">
+        var html = `${welcomeMessage}<br /><table style="border-collapse: collapse;">
       <tr style="text-align: left;">
         <th style="padding: 3px 0px;">Description</th>
         <th style="padding: 3px 0px;">Amount</th>
       </tr>${txs2}</table><br />${endingMessage}`;
 
-      await sendEmail('abby@hackclub.com', 'abby@hackclub.com', subject, html);
-      await sendEmail('max@hackclub.com', 'abby@hackclub.com', subject, html);
-      await sendEmail('christina@hackclub.com', 'abby@hackclub.com', subject, html);
+        await sendEmail('abby@hackclub.com', 'abby@hackclub.com', subject, html);
+        await sendEmail('max@hackclub.com', 'abby@hackclub.com', subject, html);
+        await sendEmail('christina@hackclub.com', 'abby@hackclub.com', subject, html);
 
-      res.status(200).send("it sent!!!! WOOOHOOOO");
-    })
+        res.status(200).send("it sent!!!! WOOOHOOOO");
+      })
+      .catch(error => {
+        console.error(error);
+        res.status(500).send("An error occurred while fetching the data.");
+      });
   } else {
     res.status(400).send("nope didn't work");
   }
@@ -77,7 +81,7 @@ function sendEmail(to, from, subject, html) {
       console.log("sent!");
       resolve(body);
     });
-  })
+  });
 }
 
 function getSubjectForDay(date) {
